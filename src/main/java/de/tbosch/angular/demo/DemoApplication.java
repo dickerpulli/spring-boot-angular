c675e61531281a45f21521013c115e5f2d75b5c0
Thomas Bosch <tbosch@gmx.de>
2020-02-13 11:59:08 +0100

    angular-demo

--- src/main/java/de/tbosch/angular/demo/DemoApplication.java
+++ src/main/java/de/tbosch/angular/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package de.tbosch.angular.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
+
+import javax.servlet.FilterConfig;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<UrlRewriteFilter> loggingFilter() {
+        FilterRegistrationBean<UrlRewriteFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new UrlRewriteFilter());
+        registrationBean.addUrlPatterns("/**/*");
+        return registrationBean;
+    }
 
 }
